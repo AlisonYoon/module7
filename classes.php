8461505d2cd9cc5a7b4ed5711d17b710e1cbf99f
AlisonYoon <politescarlett1@gmail.com>
2019-09-10 17:01:29 +0100

    class is a blueprint for object

---
+++ classes.php
@@ -0,0 +1,11 @@
+<?php
+
+#   classes : specification for what an object will look like.
+//object instantiation - make one of those.
+//creation of an object of a certain class
+//can create may objects of the same class
+
+
+#   object  (ie. remote control)
+//has stuff properties, store data  (remote control has buttons, battery...)
+//can do stuff, methods, functions  (you can turn on/off TV with it...)

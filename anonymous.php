6376e65eaf087450a7186704928a710fe29431f1
AlisonYoon <politescarlett1@gmail.com>
2019-09-10 10:36:44 +0100

    use anonymous function and array_map together

--- anonymous.php
+++ anonymous.php
@@ -22,5 +22,24 @@ $example = function ($arg) use ($message) {
 $message = 'fish';
 $example("hello");
 
+//use array_map and anonymouse function together
+$array = ['first' => 1,3,6,3,14];
+
+function addTen($value){
+    return $value + 10;
+}
+
+$result = array_map('addTen', $array);
+
+var_dump($result);
+echo '<br/>';
+
+$resultWithAnonymousFunction = array_map(
+    function ($value) {
+        return $value + 10;
+    }, $array);
+
+var_dump($resultWithAnonymousFunction);
+
 ?>
 

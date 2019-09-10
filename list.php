e7c8206c5e1dc63a739fccf97e2c6a9c8eb67fc9
AlisonYoon <politescarlett1@gmail.com>
2019-09-10 15:41:00 +0100

    list() is a language construct

---
+++ list.php
@@ -0,0 +1,19 @@
+<?php
+
+#   list() is a language construct. not a function
+//list($variable1, $variable2, ...) :array
+
+$info = ['coffee', 'brown', 'caffeine'];
+
+// Listing all the variables
+list($drink, $color, $power) = $info;
+echo "$drink is $color and $power makes it special.";
+//print "coffee is brown and caffeine makes it special."
+echo '<br/>';
+
+$info = ['coffee', 'brown', 'caffeine'];
+
+// Listing some of them
+list($drink, , $power) = $info;
+echo "$drink has $power.";
+//print "coffee has caffeine"

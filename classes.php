d453987c123f97dfbdd25664cae664434232716f
AlisonYoon <politescarlett1@gmail.com>
2019-09-11 10:13:12 +0100

    class, object, instantiation, property, method, object accessor

--- classes.php
+++ classes.php
@@ -5,7 +5,27 @@
 //creation of an object of a certain class
 //can create may objects of the same class
 
+class ClassName{}   // class
+$newobject = new ClassName();  // instantiation
 
 #   object  (ie. remote control)
 //has stuff properties, store data  (remote control has buttons, battery...)
 //can do stuff, methods, functions  (you can turn on/off TV with it...)
+//it is data type
+
+
+#   instantiation : makes one of those(objects)
+//makes an object of a certain class
+
+
+#   property
+//value, variable on an object, how data is kept on an object
+
+
+#   method
+//function on an object
+//stuff that object/class can do
+
+
+#   object accessor   : ->  single arrow
+//access properties and methods of an object

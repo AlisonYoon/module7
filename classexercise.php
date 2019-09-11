da729c06d30973dc31308220b2e05ad66d4ebd9f
AlisonYoon <politescarlett1@gmail.com>
2019-09-11 13:16:01 +0100

    class exercise - create a DB entity class done

--- classexercise.php
+++ classexercise.php
@@ -1,1 +1,24 @@
 <?php
+
+#   Create a DB entity class with autosave
+
+class DbEntity {
+    private $db;
+
+    public function __construct($dbname) {
+        $this->db = new PDO('mysql:host=192.168.20.20; dbname=' . $dbname, 'root', '');
+        $this->db->setAttribute(
+            PDO::ATTR_DEFAULT_FETCH_MODE,
+            PDO::FETCH_ASSOC);
+    }
+
+    public function sqlForId($idToSearchFor) {
+        $sql = $this->db->prepare('SELECT * FROM `users` WHERE id=' . $idToSearchFor . ';');
+        $sql->execute();
+        $results = $sql->fetch();
+        var_dump($results);
+    }
+}
+
+$findId = new DbEntity('users');
+$findId->sqlForId('24');

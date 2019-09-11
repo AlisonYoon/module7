7608b7a300037c0d7e72d185514742803f53df91
AlisonYoon <politescarlett1@gmail.com>
2019-09-11 16:47:07 +0100

    class constance

---
+++ classconstance.php
@@ -0,0 +1,40 @@
+<?php
+
+//constance : if you wanna keep using it multiple times, make constance.
+//ie. use it for DB connection.
+
+class Vegetable {
+    const BAR = 'jelly';
+
+    public function getBAR()
+    {
+        return self::BAR;
+    }
+}
+
+$test = new Vegetable();
+echo $test->getBAR();
+
+
+class Foo {
+    const BAR = 'jelly';
+    const FRUIT = 'lemon';  // can have more than 1 constance in a class
+
+    public function getBAR()
+    {
+
+        return self::BAR;
+    }
+}
+
+echo Foo::BAR;  // you can access class Foo's constance without instantiating it.
+echo '<br/>';
+echo Foo::FRUIT;
+echo '<br/>';
+
+echo Foo::class;
+
+echo '<br/>';
+
+define('LION', 'raaaa');  // Global constance 'raaaa' is the value of 'LION' global constance
+echo LION;

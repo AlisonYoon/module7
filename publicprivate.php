2c8732c49a6eb1a419079afd71f5d26597a57a57
AlisonYoon <politescarlett1@gmail.com>
2019-09-11 11:10:09 +0100

    public, private, protected example

--- publicprivate.php
+++ publicprivate.php
@@ -7,4 +7,47 @@
 #   protected : can only be read or written to in the current or child class
 //              can only be accessed in the class that defines it, current and inheriting classes.
 
-//Methods - some but 'where they can be called from'.
+//Methods - some but 'where they can be called from'
+
+class MyClass
+{
+    public $public = 'Public';
+    protected $protected = 'Protected';
+    private $private = 'Private';
+
+    function printHello()
+    {
+        echo $this->public;
+        echo $this->protected;
+        echo $this->private;
+    }
+}
+
+$obj = new MyClass();
+echo $obj->public;
+//echo $obj->protected;
+//echo $obj->private;
+//$obj->printHello();
+
+/**
+ * Define MyClass2
+ */
+class MyClass2 extends MyClass
+{
+    // We can redeclare the public and protected properties, but not private
+    public $public = 'Public2';
+    protected $protected = 'Protected2';
+
+    function printHello()
+    {
+        echo $this->public;
+        echo $this->protected;
+//        echo $this->private;
+    }
+}
+
+$obj2 = new MyClass2();
+//echo $obj2->public; // Works
+//echo $obj2->private; // Undefined
+//echo $obj2->protected; // Fatal Error
+//$obj2->printHello(); // Shows Public2, Protected2, Undefined

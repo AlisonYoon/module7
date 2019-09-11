85b602bd51ae460cbc0d90bf53a7829472ae85fa
AlisonYoon <politescarlett1@gmail.com>
2019-09-11 11:05:51 +0100

    OOP and public, private, protected

--- classes.php
+++ classes.php
@@ -8,6 +8,20 @@
 class ClassName{}   // class
 $newobject = new ClassName();  // instantiation
 
+class Jellyfish {
+    public $name;
+    public $age;
+}
+
+$squiddlyDiddly = new Jellyfish();
+$squiddlyDiddly->name = 'Ben';
+$squiddlyDiddly->age = 3;
+
+echo $squiddlyDiddly->name;
+echo '<br/>';
+echo $squiddlyDiddly->age;
+
+
 #   object  (ie. remote control)
 //has stuff properties, store data  (remote control has buttons, battery...)
 //can do stuff, methods, functions  (you can turn on/off TV with it...)
@@ -25,6 +39,21 @@ $newobject = new ClassName();  // instantiation
 #   method
 //function on an object
 //stuff that object/class can do
+
+class JellyfishTwo {
+    public $name;
+
+    function swim(){
+        return $this->name . ' is swimming...';
+    }
+
+
+}
+
+$squiddlyDiddly = new JellyfishTwo();
+$squiddlyDiddly->name = 'Ben';
+echo '<br/>';
+echo $squiddlyDiddly->swim();
 
 
 #   object accessor   : ->  single arrow
---
+++ publicprivate.php
@@ -0,0 +1,10 @@
+<?php
+#   Visibility
+
+#   public : anything can see or change the property, can be accessed anywhere
+#   private : can only be read or written to in the current class, not in any parent or child
+//            can only be accessed in the class that defines it.
+#   protected : can only be read or written to in the current or child class
+//              can only be accessed in the class that defines it, current and inheriting classes.
+
+//Methods - some but 'where they can be called from'.

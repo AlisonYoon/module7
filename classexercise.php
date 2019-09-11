d0b306eadc0c0739bf5448b0109374855b853544
AlisonYoon <politescarlett1@gmail.com>
2019-09-11 14:37:06 +0100

    auto save works

--- classexercise.php
+++ classexercise.php
@@ -2,8 +2,12 @@
 
 #   Create a DB entity class with autosave
 
-class DbEntity {
+class UserEntity {
     private $db;
+    public $name;
+    public $email;
+    public $id;
+    public  $newEmail;
 
     public function __construct($dbname) {
         $this->db = new PDO('mysql:host=192.168.20.20; dbname=' . $dbname, 'root', '');
@@ -12,13 +16,41 @@ class DbEntity {
             PDO::FETCH_ASSOC);
     }
 
+    public function setEmail($newEmail) {
+        $this->newEmail = $newEmail;
+    }
+
     public function sqlForId($idToSearchFor) {
         $sql = $this->db->prepare('SELECT * FROM `users` WHERE id=' . $idToSearchFor . ';');
         $sql->execute();
         $results = $sql->fetch();
-        var_dump($results);
+        //var_dump($results);
+        $this->name = $results["name"];
+        $this->email = $results["email"];
+        $this->id = $results["id"];
+    }
+
+    public function __destruct()
+    {
+        $sql = $this->db->prepare('UPDATE `users` SET `email` =? WHERE `id` =?;');
+        $sql->execute([$this->newEmail, $this->id]);
+        echo ($this->newEmail);
     }
 }
 
-$findId = new DbEntity('users');
-$findId->sqlForId('24');
+//$findId = new UserEntity('users');
+//$findId->sqlForId('2');
+
+$Gilles = new UserEntity('users');
+$Gilles->sqlForId('2');
+//echo($Gilles->email);
+//echo "<br>";
+//echo($Gilles->name);
+//$Gilles->email = 'carrot@carrot.com';
+echo "<br>";
+echo($Gilles->email);
+echo "<br>";
+$Gilles->setEmail('onion@carrot.com');
+unset($Gilles);
+
+

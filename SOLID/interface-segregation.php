18b6152fb0385f54efb23d2771180f322aa86d74
AlisonYoon <politescarlett1@gmail.com>
2019-09-12 15:56:25 +0100

    interface-segregation principle

---
+++ SOLID/interface-segregation.php
@@ -0,0 +1,20 @@
+<?php
+
+interface ShapeInterface
+{
+    public function area();
+}
+
+interface SolidShapeInterface {
+    public function volume();
+}
+
+class Cuboid implements ShapeInterface, SolidShapeInterface {
+    public function area() {
+        // calculate the surface area of the cuboid
+    }
+
+    public function volume() {
+        // calculate the volume of the cuboid
+    }
+}

ed9fffd8b80316e9e0f9b2b0b7fe4fa1f9256754
AlisonYoon <politescarlett1@gmail.com>
2019-09-12 16:31:47 +0100

    value-object-pattern

---
+++ design-pattern/value-object-pattern.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * -Value object pattern
+	-use an object to represent a scalar(single-dimensional) type
+	-represent a specific scalar data type
+		(ie. specific scalar data type: email address.
+			"Is it a valid email address?")
+	-provide ralidation
+	-uses the __toString() magic method
+	-means you can type hint
+		(ie. I want to use an email address)
+
+ * */
+class Email
+{
+    private $email;
+
+    public function __construct(string $email)
+    {
+        if ($this->isValid($email)) {
+            $this->email = $email;
+        } else {
+            throw new Exception();
+        }
+    }
+
+    private function isValid($email)
+    {
+        //validate email address using regex
+        return preg_match(
+            '/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD',
+            $email);
+    }
+
+    public function __toString()
+    {
+        return $this->email;
+    }
+}
+
+$email = new Email('s@a.com');
+var_dump($email);
+echo '<br/>';
+
+echo $email;
+
+echo '<br/>';
+
+$notARealEmail = new Email('utter rubbish');
+
+function doThing(Email $email)
+{
+    //do something with email
+}

4f1e5109d1f1026193905cfb564f9f6d88b81422
AlisonYoon <politescarlett1@gmail.com>
2019-09-12 16:40:18 +0100

    anti patterns

---
+++ design-pattern/anti-patterns.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Singleton
+	-most contentious pattern/anti-pattern
+	-an object that is instantiated just once
+	-breaks SOLID
+
+	---------------------------------------------
+
+	God class
+	-A class that does EVERYTHING
+	-anti pattern
+	-breaks all of SOLID
+
+	---------------------------------------------
+
+	Golden Hammer
+	-anti pattern
+	-learn something new and exciting, try and use it to solve every problem
+	-use it all the time
+ *
+ * */

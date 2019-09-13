d229f00edb509ee8d88dcbc5f0c75a987ab2befa
AlisonYoon <politescarlett1@gmail.com>
2019-09-13 09:17:13 +0100

    when overloading is accepted

--- overloading.php
+++ overloading.php
@@ -9,3 +9,5 @@ $glassJar = new Jar();
 $glassJar->contents = 'gherkins';  // contents didn't exist in the class and your'e "overloading" it after the class is defined.
 
 echo $glassJar->contents;
+
+//overloading is normally not acceptable except for when you use database and want to assign data into it.
